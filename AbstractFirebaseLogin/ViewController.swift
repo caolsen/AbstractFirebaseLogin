a396cd237ffd4d8b6920fbe34ad99a6f9c36bb4c
Christopher Olsen <olsen@one20.com>
2018-03-09 12:23:04 -0600

    Initial Commit

---
+++ AbstractFirebaseLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AbstractFirebaseLogin
+//
+//  Created by Christopher Olsen on 3/9/18.
+//  Copyright © 2018 Christopher Olsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
